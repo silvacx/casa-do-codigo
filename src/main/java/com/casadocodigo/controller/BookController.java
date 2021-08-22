8f786b94afaae83dd9f138e31b0077371addd040
Camila Silva <milanasc12@gmail.com>
2021-08-21 23:50:54 -0300

    book list

--- src/main/java/com/casadocodigo/controller/BookController.java
+++ src/main/java/com/casadocodigo/controller/BookController.java
@@ -5,14 +5,13 @@ import com.casadocodigo.model.Book;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "book")
@@ -28,4 +27,17 @@ public class BookController {
         entityManager.persist(createBook);
         return ResponseEntity.status(HttpStatus.OK).body(createBook);
     }
+
+    @GetMapping
+    public ResponseEntity<?> listBooks() {
+        try {
+            Query query = entityManager.createQuery("SELECT b FROM Book b", Book.class);
+            List<Book> list = query.getResultList();
+            return ResponseEntity.status(HttpStatus.OK).body(list);
+        } catch (Exception err) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+
 }
